cde9d1c31b05251e3d7f3cd40984f417ef82443f
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2024-10-25 12:34:18 +0500

    q7.c

---
+++ q7.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int first[3][3], second[3][3], result[3][3] = {0};
+
+    printf("Enter elements of the first 3x3 matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &first[i][j]);
+        }
+    }
+
+    printf("Enter elements of the second 3x3 matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &second[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                result[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
+
+    printf("Resultant matrix after multiplication:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
