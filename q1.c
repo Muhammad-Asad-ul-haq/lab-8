37b149e624aeb74b793195333644cc1349ec5f75
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2024-10-25 12:17:42 +0500

    q1.c

--- q1.c
+++ q1.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
-int main() 
-{
-int i, j;
-for (i=1; i<=4; i++)
-{ 
-for (j=1; j<=i; j++) 
-{
-printf("%d ",j);
-}
-printf("\n");
-}
-return 0;
+
+int main() {
+    int s, i, j;
+
+   
+    printf("Enter the size of the Matrix: ");
+    scanf("%d", &s);
+
+    int matrix[s][s];
+
+    printf("Enter the Elements of the Matrix:\n");
+    for (i = 0; i < s; i++)
+    {
+        for (j = 0; j < s; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Diagonal Elements are:\n");
+    for (i = 0; i < s; i++)
+    {
+        printf("%d ", matrix[i][i]);
+    }
+    printf("\n");
+
+    return 0;
 }
